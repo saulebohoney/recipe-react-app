3be1c57e9411abec3531068fff68556e9f70e318
Sonja Duric <duric.sonja@gmail.com>
2017-06-21 17:27:05 -0400

    all things working and complete

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -8,50 +8,41 @@ export default class LiveSearch extends React.Component{
         super(props);
         this.state = {
           searchValueName: '',
-          searchValueIngredients: '',
           searchRecipes:props.recipes,
-          ingredients:'false',
+          mode:'name',
         }
     }
 
+   
     render(){
+        let label="Recipes";
+        let tempRecipes;
 
-        // let temp;
-  
-        // if(this.state.ingredients)
-        // {
-        //     temp = this.state.searchRecipes;
-        // }
-        // else{
-
-           
-        // }
-
-        const tempRecipes1 = this.state.searchRecipes.filter( recipe => {
-                return recipe.name.indexOf(this.state.searchValueName) > -1;
-            });
-
-        const tempRecipes2 = this.state.searchRecipes.filter( recipe => {
+        if(this.state.mode === 'ingredients')
+       {
+            tempRecipes = this.state.searchRecipes.filter( recipe => {
                     const innerArray = recipe.ingredients.filter( ingredient =>{
-                        return ingredient.name.indexOf(this.state.searchValueIngredients) > -1});
+                        return ingredient.name.indexOf(this.state.searchValueName) > -1});
                     return innerArray.length > 0;
             });
-
-        return (<div>
-            <SearchForm label="Recipe" value={this.state.searchValueName} 
-            handleChange={searchInput => this.setState({searchValueName:searchInput})} />
-            <RecipesList recipes={tempRecipes1} />
-            <SearchForm label="Ingredients" value={this.state.searchValueIngredients} 
-            handleChange={searchInput2 => this.setState({searchValueIngredients:searchInput2})} />
-            <RecipesList recipes={tempRecipes2} />
-
-            <Picker/>
-
-            {/*<label="Recipe" value={this.state.searchValueName} 
-            handleChange={searchInput => this.setState({searchValueName:searchInput})} />
-            <RecipesList recipes={tempRecipes1} />*/}
-        </div>
+            label = "Ingredients";
+       }
+        else{
+            tempRecipes = this.state.searchRecipes.filter( recipe => {
+                return recipe.name.indexOf(this.state.searchValueName) > -1;
+            });
+        }
+        
+        return (
+            <div>
+                <Picker handleChange={mode => this.setState({mode})}/>
+                <SearchForm label={label} value={this.state.searchValueName} 
+                handleChange={searchInput => this.setState({searchValueName:searchInput})} />
+                <RecipesList recipes={tempRecipes} />
+            </div>
+           
         );
+       
     }
 }
 
--- src/components/picker.js
+++ src/components/picker.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Picker(props){
     return (
-        <div onChange={}>
+        <div onChange={(e) => props.handleChange(e.target.value)}>
           
           What would you like to search by?
-          <input type="radio" name="search" value="true"/><label>Recipe Name</label>
-          <input type="radio" name="search" value="false"/><label>Ingredients</label>
+          <input type="radio" name="search" value="name"/><label>Recipe Name</label>
+          <input type="radio" name="search" value="ingredients"/><label>Ingredients</label>
             {/*<label>Search {props.label}</label>
             <input type='search' placeholder='Pizza'  onChange={(e) => props.handleChange(e.target.value)} />*/}
 
